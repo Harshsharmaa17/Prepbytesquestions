6bf398ad1d815992c011b0dcdb2a8d5bab3f39d4
Harsh Sharma <110018503+Harshsharmaa17@users.noreply.github.com>
2022-12-23 16:33:32 +0530

    Create Victory.cpp

---
+++ Victory.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  int t;
+  cin >> t;
+  while(t-->0)
+  {
+    int a;
+    int b;
+    cin >>  a >> b;
+ if(a == b)
+ {
+   cout << a << endl;
+ }
+ else 
+ {
+   cout << "1" << endl;
+ }
+    
+  }
+  return 0;
+}
