5351e7c8416d6acc508df7ac5f76571b0ba08f41
Harsh Sharma <110018503+Harshsharmaa17@users.noreply.github.com>
2022-12-23 23:24:12 +0530

    Create RangeEven.cpp

---
+++ RangeEven.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  //write your code here
+  int t;
+  cin >> t;
+  while(t-->0)
+  {
+    int a;
+    cin >> a;
+    for(int i=1;i<=a;i++)
+    {
+      if(i%2==0)
+      {
+        cout << i << " ";
+      }
+      
+    }cout << endl;
+  }
+  return 0;
+}
